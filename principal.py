ac85e5ce5ba9099a10eb7c4739a870c28cb2e3bf
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-10-07 10:04:05 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,3 +1,26 @@
 import streamlit as st
-
+#import hydralit_components as hc
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.font_manager as font_manager
+import mplsoccer
+from mplsoccer import Pitch, VerticalPitch, FontManager
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial import ConvexHull
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patheffects as path_effects
+from scipy.ndimage import gaussian_filter
+import seaborn as sns
+from matplotlib import colors as mcolors
+import requests
+#from PIL import Image
+from matplotlib.patches import Rectangle
+import math
 st.title("PRINCIPAL")

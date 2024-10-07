a54b3e1bf1cfa8c6c8ff17542342f3213f525af8
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-10-07 10:26:41 -0500

    Update winstats.py

--- cont/winstats.py
+++ cont/winstats.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from streamlit_image_select import image_select
+
+
+img = image_select(
+    label="Select a cat",
+    images=[
+        "https://bagongkia.github.io/react-image-picker/0759b6e526e3c6d72569894e58329d89.jpg",
+        "https://bagongkia.github.io/react-image-picker/0759b6e526e3c6d72569894e58329d89.jpg"
+    ],
+    captions=["A cat", "Guess what, a cat..."],
+)
+st.write(img)
 
 st.header("WIN STATS")

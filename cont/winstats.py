20afe7da10c250706d680afff578dae952c0d3ff
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-10-19 10:55:47 -0500

    Update winstats.py

--- cont/winstats.py
+++ cont/winstats.py
@@ -11,11 +11,13 @@ image1 = Image.open("Resources/Img/wyscoutlogo.png")
 image2 = Image.open("Resources/Img/optalogo.png")
 image3 = Image.open("Resources/Img/fbreflogo.png")
 image4 = Image.open("Resources/Img/winstatslogo.png")
+image5 = Image.open("Resources/Img/FootballDivisionWorldwide-removebg-preview.png")
 
 link_url1 = "https://wyscoutdata.streamlit.app"
 link_url2 = "https://opproccesdata.streamlit.app"
 link_url3 = "https://fbrefdata.streamlit.app"
 link_url4 = "https://winstats-chilefem.streamlit.app"
+link_url5 = "https://agencyfd-winstats.streamlit.app"
 
 # Obtener dimensiones de la segunda imagen
 new_width, new_height = image2.size  # Ancho y alto de la segunda imagen
@@ -37,6 +39,12 @@ if resized_image4.mode == "RGBA":
 resized_image4.save("Resources/Img/resized_fbreflogo.png")  # Cambia el nombre según necesites
 resized_image4.show()
 
+resized_image5 = image5.resize((new_width, new_height), Image.LANCZOS)
+if resized_image5.mode == "RGBA":
+    resized_image5 = resized_image5.convert("RGB")
+resized_image5.save("Resources/Img/resized_fbreflogo.png")  # Cambia el nombre según necesites
+resized_image5.show()
+
 
 def image_to_base64(image):
     buffered = io.BytesIO()
@@ -52,6 +60,7 @@ image1_base64 = image_to_base64(image2)
 image2_base64 = image_to_base64(resized_image1)
 image3_base64 = image_to_base64(resized_image2)
 image4_base64 = image_to_base64(resized_image4)
+image5_base64 = image_to_base64(resized_image5)
 
 
 imgmn01, imgmn02, imgmn03 = st.columns(3)
@@ -94,6 +103,17 @@ with imgmn04:
         <a href="{link_url4}" target="_blank">
             <div style="border: 1px solid #9F9F9F; padding: 5px; display: inline-block;">
                 <img src="{image4_base64}" style="width: 100%;">
+            </div>
+        </a>
+        """,
+        unsafe_allow_html=True
+    )
+with imgmn05:
+    st.markdown(
+        f"""
+        <a href="{link_url5}" target="_blank">
+            <div style="border: 1px solid #9F9F9F; padding: 5px; display: inline-block;">
+                <img src="{image5_base64}" style="width: 100%;">
             </div>
         </a>
         """,
